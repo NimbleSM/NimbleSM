a77d91f1cebde41049cf0ba8156e5fcadc3b7778
JAntonioPerezSandia <jorpere@sandia.gov>
2019-01-17 16:39:51 -0700

    Added isFromParent and isFromChild methods to DataChannel; improved formatting

--- src/mpi-buckets/src/DataChannel.h
+++ src/mpi-buckets/src/DataChannel.h
@@ -6,7 +6,6 @@
 
 struct DataChannel
 {
-
     constexpr static auto any_source = MPI_ANY_SOURCE;
 
     MPI_Comm comm;
@@ -24,17 +23,19 @@ struct DataChannel
         MPI_Comm_size(comm, &size);
         return size;
     }
-    auto Iawait(int sender) const -> MPI_Request {
-        return Irecv(NullOf<int>(), 0, sender); 
+    auto Iawait(int sender) const -> MPI_Request
+    {
+        return Irecv(NullOf<int>(), 0, sender);
     }
     /**
      * @brief Fulfill a pending await on another rank
-     * 
-     * @param reciever 
+     *
+     * @param reciever
      * @return void
      */
-    auto notify(int reciever) const -> void {
-        MPI_Send(NullOf<int>(), 0, MPI_INT, reciever, tag, comm); 
+    auto notify(int reciever) const -> void
+    {
+        MPI_Send(NullOf<int>(), 0, MPI_INT, reciever, tag, comm);
     }
     template <class T>
     auto Irecv(T* dataBuffer, size_t count, int source) const -> MPI_Request
@@ -51,9 +52,11 @@ struct DataChannel
     {
         return Irecv(message.data(), message.size(), source);
     }
-    template <class T> 
-    auto recv(T* dataBuffer, size_t count, int source) const -> void {
-        MPI_Recv(dataBuffer, count * sizeof(T), MPI_BYTE, source, tag, comm, nullptr); 
+    template <class T>
+    auto recv(T* dataBuffer, size_t count, int source) const -> void
+    {
+        MPI_Recv(
+            dataBuffer, count * sizeof(T), MPI_BYTE, source, tag, comm, nullptr);
     }
 
     template <class T>
@@ -93,21 +96,47 @@ struct DataChannel
     {
         return IrecvAny(message.data(), message.size());
     }
-    template <class F, class... ExtraArgs> 
-    auto onChildRanks(F&& func, ExtraArgs&&... args) const -> void {
-        const int num_ranks = commSize(); 
-        const long long my_rank = commRank(); 
-        const auto child_0 = (my_rank + 1) * 2 - 1; 
-        const auto child_1 = (my_rank + 1) * 2; 
-        if(child_0 < num_ranks) func(args..., *this, (int)child_0);
-        if(child_1 < num_ranks) func(args..., *this, (int)child_1); 
+    template <class F, class... ExtraArgs>
+    auto onChildRanks(F&& func, ExtraArgs&&... args) const -> void
+    {
+        int const       num_ranks = commSize();
+        long long const my_rank   = commRank();
+        auto const      child_0   = (my_rank + 1) * 2 - 1;
+        auto const      child_1   = (my_rank + 1) * 2;
+        if (child_0 < num_ranks)
+        {
+            func(args..., *this, (int)child_0);
+        }
+        if (child_1 < num_ranks)
+        {
+            func(args..., *this, (int)child_1);
+        }
     }
     template <class F, class... ExtraArgs>
-    auto onParentRanks(F&& func, ExtraArgs&&... args) const -> void {
-        const int my_rank = commRank(); 
-        if(my_rank != 0) {
-            const int parent = (my_rank - 1) / 2; 
-            func(args..., *this, parent); 
+    auto onParentRanks(F&& func, ExtraArgs&&... args) const -> void
+    {
+        int const my_rank = commRank();
+        if (my_rank != 0)
+        {
+            const int parent = (my_rank - 1) / 2;
+            func(args..., *this, parent);
         }
+    }
+    auto isFromParent(MPI_Status const& status) const -> bool
+    {
+        return isFromParent(status.MPI_SOURCE);
+    }
+    auto isFromChild(MPI_Status const& status) const -> bool
+    {
+        return isFromChild(status.MPI_SOURCE);
+    }
+    auto isFromParent(int sender) const -> bool
+    {
+        int const parent = (commRank() - 1) / 2;
+        return sender == parent;
+    }
+    auto isFromChild(int sender) const -> bool
+    {
+        return (sender - 1) / 2 == commRank();
     }
 };
