b315c8fa7cc5614f95ee451c7325277a7fc180fd
JAntonioPerezSandia <jorpere@sandia.gov>
2019-02-28 05:22:19 -0700

    Variant implementation almost complete

--- src/parser/src/variant.hpp
+++ src/parser/src/variant.hpp
@@ -1,8 +1,10 @@
 #include <array>
 #include <cstddef>
-#include <string>
 #include <type_traits>
 #include <utility>
+
+template <class F, class... V>
+void visit(F&& func, V&&... variants);
 
 template <class...>
 class TypeSet
@@ -10,13 +12,13 @@ class TypeSet
    public:
     constexpr static size_t size = 0;
 };
-struct ForwardWrapper
+struct Box
 {
     template <class T>
-    static ForwardWrapper wrap(T&& thing)
+    static Box wrap(T&& thing)
     {
         using PureT = typename std::decay<T>::type;
-        return ForwardWrapper{const_cast<PureT*>(&thing)};
+        return Box{const_cast<PureT*>(&thing)};
     }
     void* addr;
     template <class T>
@@ -26,7 +28,21 @@ struct ForwardWrapper
         return const_cast<T>(*reinterpret_cast<PureT*>(addr));
     }
 };
+template <class T>
+using Wrap_t = Box;
 
+template <class F, class... Conv>
+struct FG
+{
+    static void app(F func, Wrap_t<Conv>... value)
+    {
+        func(value.template as<Conv>()...);
+    }
+    static void app_boxed(Box func, Conv... values)
+    {
+        func.as<F>()(std::forward<Conv>(values)...);
+    }
+};
 template <class T>
 constexpr T const& recursive_index(T const& value)
 {
@@ -72,18 +88,26 @@ union Union;
 template <class T>
 union Union<T>
 {
+    constexpr static bool trivially_destructible
+        = std::is_trivially_destructible<T>::value;
     Union(T const& thing) : value{thing} {}
     Union(T&& thing) : value{std::move(thing)} {}
 
     T    value;
     void init(T const& new_value) { new (&value) T(new_value); }
     void assign(T const& new_value) { value = new_value; }
+    void init(T&& new_value) { new (&value) T(std::move(new_value)); }
+    void assign(T&& new_value) { value = std::move(new_value); }
     ~Union(){};
 };
 // General case
 template <class T, class... Rest>
 union Union<T, Rest...>
 {
+    constexpr static bool trivially_destructible
+        = std::is_trivially_destructible<T>::value
+        && Union<Rest...>::trivially_destructible;
+
     T              value;
     Union<Rest...> rest;
     Union() {}
@@ -93,32 +117,48 @@ union Union<T, Rest...>
     }
     Union(T const& thing) : value{thing} {}
     Union(T&& thing) : value{std::move(thing)} {}
+    void init(T&& new_value) { new (&value) T(std::move(new_value)); }
+    void assign(T&& new_value) { value = std::move(new_value); }
     void init(T const& new_value) { new (&value) T(new_value); }
     void assign(T const& new_value) { value = new_value; }
     template <class Q>
-    void init(Q const& new_value)
+    void init(Q&& new_value)
     {
-        rest.init(new_value);
+        rest.init(std::forward<decltype(new_value)>(new_value));
     }
     template <class Q>
-    void assign(Q const& new_value)
+    void assign(Q&& new_value)
     {
-        rest.assign(new_value);
+        rest.assign(std::forward<decltype(new_value)>(new_value));
     }
     ~Union(){};
 };
 
 template <class T>
-void destroy_as(void* i)
+void del(void* i)
 {
     ((T*)i)->~T();
+}
+template <class U, class T>
+void init_as(U& u, Box t)
+{
+    u.init(t.as<T>());
+}
+template <class U, class T>
+void assign_as(U& u, Box t)
+{
+    u.assign(t.as<T>());
 }
 
 template <class... T>
 struct variant
 {
-    Union<T...> value;
-    size_t      index;
+    using assigner_t = void (*)(Union<T...>&, Box);
+    Union<T...>           value;
+    constexpr static bool trivially_destructible
+        = Union<T...>::trivially_destructible;
+    constexpr static bool call_destructor = !trivially_destructible;
+    size_t                index;
     template <class Q>
     variant(Q&& thing)
       : value{std::forward<decltype(thing)>(thing)}
@@ -128,8 +168,52 @@ struct variant
                           < sizeof...(T),
                       "Bad type");
     }
-    variant(variant const& v) = delete;
-    variant(variant&& other)  = delete;
+    variant(variant const& other)
+    {
+        constexpr static assigner_t lookup[]{init_as<Union<T...>, T const&>...};
+        lookup[other.index](value, Box{&other.value});
+        index = other.index;
+    }
+    variant(variant&& other)
+    {
+        constexpr static assigner_t lookup[]{init_as<Union<T...>, T&&>...};
+        lookup[other.index](value, Box{&other.value});
+        index = other.index;
+    }
+    auto operator=(variant const& thing) -> variant&
+    {
+        constexpr static assigner_t init_lookup[]{init_as<Union<T...>, T const&>...};
+        constexpr static assigner_t assign_lookup[]{
+            assign_as<Union<T...>, T const&>...};
+
+        if (index == thing.index)
+        {
+            assign_lookup[index](value, Box{&thing.value});
+        }
+        else
+        {
+            this->~variant();
+            init_lookup[index](value, Box{&thing.value});
+        }
+        return *this;
+    }
+    auto operator=(variant&& thing) -> variant&
+    {
+        constexpr static assigner_t init_lookup[]{init_as<Union<T...>, T&&>...};
+        constexpr static assigner_t assign_lookup[]{
+            assign_as<Union<T...>, T const&>...};
+
+        if (index == thing.index)
+        {
+            assign_lookup[index](value, Box{&thing.value});
+        }
+        else
+        {
+            this->~variant();
+            init_lookup[index](value, Box{&thing.value});
+        }
+        return *this;
+    }
     template <class Q>
     auto operator=(Q&& thing) -> variant&
     {
@@ -138,33 +222,34 @@ struct variant
         static_assert(Q_index < sizeof...(T), "Item not in variant");
         if (Q_index == index)
         {
-            ((Qplain&)value) = thing;
+            ((Qplain&)value) = std::forward<decltype(thing)>(thing);
         }
         else
         {
-            ~variant();
-            value.init(thing);
+            this->~variant();
+            value.init(std::forward<decltype(thing)>(thing));
+            index = Q_index;
         }
+        return *this;
     }
     ~variant()
     {
-        using destructor_t = void (*)(void*);
-        constexpr static destructor_t lookup[]{destroy_as<T>...};
-        lookup[index](&value);
+        if (call_destructor)
+        {
+            using destructor_t = void (*)(void*);
+            constexpr static destructor_t lookup[]{del<T>...};
+            lookup[index](&value);
+        }
     }
-};
-
-template <class T>
-using Wrap_t = ForwardWrapper;
-
-template <class F, class... Conv>
-struct FuncGroup
-{
-    static void convert_apply(F func, Wrap_t<Conv>... value)
+    template <class... In>
+    void operator()(In&&... inputs)
     {
-        func(value.template as<Conv>()...);
+        using caller_t = void (*)(Box, decltype(inputs)...);
+        constexpr static caller_t lookup[]{FG<T, decltype(inputs)...>::app_boxed...};
+        lookup[index](std::forward<decltype(inputs)>(inputs)...);
     }
 };
+
 
 
 template <class...>
@@ -174,10 +259,7 @@ class VTable<Func, ParamList<T...>>
 {
    public:
     using table_t = void (*)(Func, Wrap_t<T>...);
-    constexpr static auto table() -> table_t
-    {
-        return FuncGroup<Func, T...>::convert_apply;
-    }
+    constexpr static auto table() -> table_t { return FG<Func, T...>::app; }
 };
 template <class Func,
           template <class...> class ParamList,
@@ -236,5 +318,5 @@ void visit(F&& func, V&&... variants)
     constexpr static auto lookup
         = VTable<F&, TypeSet<>, decltype(variants)...>::table();
     const auto func_ptr = recursive_index(lookup, variants.index...);
-    func_ptr(func, ForwardWrapper::wrap(variants.value)...);
+    func_ptr(func, Box::wrap(variants.value)...);
 }
