ac3b6f16c3af94e1e126be48be0e68534e5ba4c7
Nicolas Morales <nmmoral@sandia.gov>
2020-04-08 10:58:39 -0700

    fix exodus code not being added in exodus builds

--- cmake/FindExodus.cmake
+++ cmake/FindExodus.cmake
@@ -32,7 +32,7 @@ if (Exodus_FOUND)
     add_library(Exodus::ExodusII UNKNOWN IMPORTED)
     set_target_properties(Exodus::ExodusII PROPERTIES
       IMPORTED_LOCATION "${exodus_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${exodus_INCLUDE_DIR}"
+      INTERFACE_INCLUDE_DIRECTORIES "${exodus_INCLUDE_DIR};${SEACASExodus_TPL_INCLUDE_DIRS}"
       INTERFACE_LINK_LIBRARIES "${SEACASExodus_TPL_LIBRARIES}"
     )
   endif()
