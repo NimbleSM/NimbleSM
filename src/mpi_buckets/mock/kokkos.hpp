08a1389bbcf23460aff06b64405f496499de496a
uhetmaniuk <ulrich.hetmaniuk@ng-analytics.com>
2020-07-05 22:56:05 -0700

    Delete kokkos.hpp

--- src/mpi_buckets/mock/kokkos.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-struct KokkosMock
-{
-    std::vector<double> values;
-    auto                extent(int) const -> size_t { return values.size(); }
-    auto operator()(int index) const -> double const& { return values[index]; }
-};
