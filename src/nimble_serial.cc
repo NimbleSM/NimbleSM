1e9b7f0851e2649e389ccc8353a8891c9707a13a
Jean-Laurent Picard <jean-laurent.picard@ng-analytics.com>
2020-07-13 08:59:23 +0200

    ArborX linked and included
    Modified cmake files to include ArborX if the HAVE_ARBORX is set. Added a simple ArborX::Point object in nimble_serial.cc for test purposes.

--- src/nimble_serial.cc
+++ src/nimble_serial.cc
@@ -64,6 +64,11 @@
 #include "nimble_uq.h"
 #endif
 
+#ifdef NIMBLE_HAVE_ARBORX
+#include <ArborX.hpp>
+#endif
+
+
 int ExplicitTimeIntegrator(nimble::Parser & parser,
                            nimble::GenesisMesh & mesh,
                            nimble::DataManager & data_manager,
@@ -121,6 +126,11 @@ int NimbleSerialMain(std::shared_ptr<nimble::MaterialFactory> material_factory,
                      std::shared_ptr<nimble::Parser> parser,
                      const std::string& input_deck_name) {
   parser->Initialize(input_deck_name);
+
+#ifdef NIMBLE_HAVE_ARBORX
+  // Ensuring that ArborX is properly included and linked, and that objects can be instantiated
+  ArborX::Point test;
+#endif
 
   // Read the mesh
   std::string genesis_file_name = parser->GenesisFileName();
