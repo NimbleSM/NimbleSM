1568aa2737a3344187fbcaa73d7154b405b9503a
Jean-Laurent Picard <jean-laurent.picard@ng-analytics.com>
2020-07-13 08:59:23 +0200

    ArborX linked and included
    Modified cmake files to include ArborX if the HAVE_ARBORX is set. Added a simple ArborX::Point object in nimble_serial.cc for test purposes.

--- src/contact/serial/serial_contact_manager.h
+++ src/contact/serial/serial_contact_manager.h
@@ -45,7 +45,6 @@
 #define NIMBLE_SERIAL_CONTACT_MANAGER_H
 
 #include "../../nimble_contact_manager.h"
-#include "../../nimble_mpi.h"
 
 namespace nimble {
   class SerialContactManager : public ContactManager {
