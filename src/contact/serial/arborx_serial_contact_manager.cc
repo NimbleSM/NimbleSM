2b6b0d11ca50770d31ff054d7dcf56d5a98f6c7a
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-07-16 13:33:12 -0700

    Fix linking issue for ArborX.

--- src/contact/serial/arborx_serial_contact_manager.cc
+++ src/contact/serial/arborx_serial_contact_manager.cc
@@ -1,7 +1,57 @@
-#include "arborx_serial_contact_manager.h"
+/*
+//@HEADER
+// ************************************************************************
+//
+//                                NimbleSM
+//                             Copyright 2018
+//   National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+//
+// Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government
+// retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL NTESS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?  Contact David Littlewood (djlittl@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#ifdef NIMBLE_HAVE_ARBORX
+
 #include <fstream>
 
+#include <ArborX.hpp>
+
+#include "arborx_serial_contact_manager.h"
+
 namespace nimble {
+
+  using bvh_type = ArborX::BVH< nimble_kokkos::kokkos_device_memory_space >;
 
   ArborXSerialContactManager::ArborXSerialContactManager(std::shared_ptr<ContactInterface> interface):
   SerialContactManager(interface)
@@ -18,3 +68,5 @@ namespace nimble {
 //  ArborXSerialContactManager::~ArborXSerialContactManager() = default;
 
 }
+
+#endif
