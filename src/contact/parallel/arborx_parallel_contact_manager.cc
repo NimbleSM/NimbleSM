b268fc37ef45956bb86cfe1d1a81381e535d2e4b
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-08-04 14:36:23 -0700

    (#60) Fix flagging when running in parallel

--- src/contact/parallel/arborx_parallel_contact_manager.cc
+++ src/contact/parallel/arborx_parallel_contact_manager.cc
@@ -161,7 +161,8 @@ namespace nimble {
 
     for (size_t ijk = 0; ijk < contact_nodes_d_.extent(0); ++ijk) {
       for (int j = offset(ijk); j < offset(ijk+1); ++j) {
-        contact_faces_d_(indices(j)).set_contact_status(true);
+        if (ranks(j) == m_rank)
+          contact_faces_d_(indices(j)).set_contact_status(true);
       }
     }
 
