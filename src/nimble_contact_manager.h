e3774ca1a511b2402dce9d91a4cccfd7ab151095
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-08-04 15:29:02 -0700

    (#60) Add contact force

--- src/nimble_contact_manager.h
+++ src/nimble_contact_manager.h
@@ -300,6 +300,9 @@ public:
     /// \brief Zero the contact forces
     void zeroContactForce();
 
+  /// \brief Zero the contact forces
+  void zeroContactForce();
+
 protected:
  
   PenaltyContactEnforcement enforcement;
