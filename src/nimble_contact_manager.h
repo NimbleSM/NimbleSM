4409b1b640f8937679832deef2f9c38b377617a5
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-08-10 20:08:32 -0700

    (#60) Fix merge error.

--- src/nimble_contact_manager.h
+++ src/nimble_contact_manager.h
@@ -300,12 +300,6 @@ public:
     /// \brief Zero the contact forces
     void zeroContactForce();
 
-  /// \brief Zero the contact forces
-  void zeroContactForce();
-
-  /// \brief Zero the contact forces
-  void zeroContactForce();
-
 protected:
  
   PenaltyContactEnforcement enforcement;
