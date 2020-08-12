92925b8946b6508678f2ca1b3a99ee182c96084a
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-08-11 22:34:26 -0700

    (#60) Fix compilation error after rebasing.

--- src/contact/parallel/arborx_parallel_contact_manager.cc
+++ src/contact/parallel/arborx_parallel_contact_manager.cc
@@ -155,7 +155,6 @@ namespace nimble {
 
     //--- Set vector to store force
     ContactManager::zeroContactForce();
-    contact_interface->SetContactForce(force_d_);
 
     // Reset the contact_status flags
     for (size_t jj = 0; jj < contact_faces_d_.extent(0); ++jj)
@@ -170,23 +169,21 @@ namespace nimble {
     //
     double gap = 0.0;
     double normal[3] = {0., 0., 0.};
-    ContactManager::PROJECTION_TYPE flag = UNKNOWN;
-    ContactEntity::vertex projected;
+    bool inside = false;
+    double facet_coordinates[3] = {0., 0., 0.};
     for (size_t inode = 0; inode < contact_nodes_d_.extent(0); ++inode) {
       auto &myNode = contact_nodes_d_(inode);
       for (int j = offset(inode); j < offset(inode+1); ++j) {
         if (ranks(j) != m_rank)
           continue;
-        //
-        //--- Determine whether the node is projected inside the triangular face
-        //
         auto &myFace = contact_faces_d_(indices(j));
-        ContactManager::SimpleClosestPointProjectionSingle(myNode, myFace,
-            &flag, &projected, gap, &normal[0]);
-        if ((flag != UNKNOWN) && (gap < 0.0)) {
+        //--- Determine whether the node is projected inside the triangular face
+        ContactManager::Projection(myNode, myFace,
+            inside, gap, &normal[0], &facet_coordinates[0]);
+        if (inside) {
           contact_faces_d_(indices(j)).set_contact_status(true);
           contact_nodes_d_(inode).set_contact_status(true);
-          contact_interface->EnforceNodeFaceInteraction(myNode, myFace, 3, gap, normal, projected.coords_);
+          EnforceNodeFaceInteraction(myNode, myFace, gap, normal, facet_coordinates, force_d_);
         }
       }
     }
