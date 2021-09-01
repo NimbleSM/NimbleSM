90cb962934db40f129304f21ee824c0186a3c2d7
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2021-08-31 22:34:15 -0600

    #184 - Fix compilation

--- src/nimble_utils.h
+++ src/nimble_utils.h
@@ -520,7 +520,7 @@ Invert_Full33(const ScalarT* mat, ScalarT* inv)
   ScalarT minor8 = mat[K_F_XX] * mat[K_F_YY] - mat[K_F_XY] * mat[K_F_YX];
   ScalarT det    = mat[K_F_XX] * minor0 - mat[K_F_XY] * minor1 + mat[K_F_XZ] * minor2;
 
-  NIMBLE_DEBUG_ASSERT(det >= 0.0, "Error in Invert_Full33(), singular matrix");
+  NIMBLE_ASSERT(det >= 0.0, "Error in Invert_Full33(), singular matrix");
 
   inv[K_F_XX] = minor0 / det;
   inv[K_F_XY] = -1.0 * minor3 / det;
@@ -1139,8 +1139,8 @@ Log_Rotation_Pi(const ScalarT* const rotation, ScalarT* const log_rotation)
     norm = normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2];
     if (norm > 0.0) { norm = std::sqrt(norm); }
 
-    NIMBLE_DEBUG_ASSERT(norm >= machine_epsilon, "\n**** Error in Log_Rotation_Pi(), cannot"
-                        " determine rotation vector.");
+    NIMBLE_ASSERT(norm >= machine_epsilon, "\n**** Error in Log_Rotation_Pi(), cannot"
+                  " determine rotation vector.");
   }
 
   normal[0] /= norm;
