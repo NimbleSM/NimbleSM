fc0f1050265808c19420ff127dba035b84c7c8eb
uhetmaniuk <ulrich.hetmaniuk@ng-analytics.com>
2021-09-02 15:44:30 -0600

    Delete test directory

--- test/_wip/uq_wave_in_bar/postprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-scrape_1D.py uq_wave_in_bar.serial.e
-rm solution.dat
-if [ ! -e solution.dat ]; then
-analytic_solution.py x.dat t.dat > solution.log
-fi 
-paste solution.dat uq_wave_in_bar.dat > u.dat
--- test/_wip/uq_wave_in_bar/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-../../../build_NimbleSM_UQ/src/NimbleSM_Serial uq_wave_in_bar.in
-#scrape_1D.py uq_wave_in_bar.serial.e
-#analytic_solution.py x.dat t.dat > solution.log
-#paste solution.dat uq_wave_in_bar.dat > u.dat
-exodiff -f uq_wave_in_bar.exodiff uq_wave_in_bar.serial.e uq_wave_in_bar.gold.e > test.log ; tail -n2 test.log
