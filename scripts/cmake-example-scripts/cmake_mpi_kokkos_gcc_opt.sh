f1d391efda7ba09e3412a79ad3ced5c175747a34
uhetmaniuk <ulrich.hetmaniuk@ng-analytics.com>
2021-09-18 15:31:08 -0600

    Update cmake_mpi_kokkos_gcc_opt.sh

--- scripts/cmake-example-scripts/cmake_mpi_kokkos_gcc_opt.sh
+++ scripts/cmake-example-scripts/cmake_mpi_kokkos_gcc_opt.sh
@@ -8,10 +8,10 @@ cmake \
 -D CMAKE_CXX_EXTENSIONS:BOOL=OFF \
 -D EXODUS_INCLUDE_DIR:PATH=/path/to/seacas/include \
 -D EXODUS_LIB_DIR:PATH=/path/to/seacas/lib \
--D HAVE_KOKKOS:BOOL=ON \
+-D NimbleSM_ENABLE_KOKKOS:BOOL=ON \
 -D KOKKOS_ROOT_DIR:PATH=/path/to/kokkos \
 -D KOKKOS_ENABLE_LIBRT:BOOL=OFF \
--D USE_PURE_MPI:BOOL=ON \
+-D NimbleSM_ENABLE_MPI:BOOL=ON \
 ../../Nimble/
 
 
