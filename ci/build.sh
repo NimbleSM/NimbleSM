f796c37722adb719e574888663e685bedebb82b9
Nicolas Morales <nmmoral@sandia.gov>
2021-11-29 10:50:53 -0800

    ci: fix build path in script

--- ci/build.sh
+++ ci/build.sh
@@ -14,5 +14,5 @@ cmake -DCMAKE_BUILD_TYPE=Release \
      -DCMAKE_CXX_FLAGS="-Werror" \
      -DNimbleSM_ENABLE_UNIT_TESTS=ON \
      -DNimbleSM_ENABLE_MPI=$NIMBLESM_ENABLE_MPI
-     -S /opt/src/NimbleSM -B /opt/builds/NimbleSM
-cmake --build /opt/builds/NimbleSM --parallel $(nproc)
+     -S /opt/src/NimbleSM -B /opt/build/NimbleSM
+cmake --build /opt/build/NimbleSM --parallel $(nproc)
