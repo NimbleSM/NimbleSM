122e229ed95b66513f25fe380a5a0c87d38b091c
Nicolas Morales <nmmoral@sandia.gov>
2021-11-29 10:53:35 -0800

    ci: fix error in build bash script

--- ci/build.sh
+++ ci/build.sh
@@ -13,6 +13,6 @@ cmake -DCMAKE_BUILD_TYPE=Release \
      -DCMAKE_CXX_COMPILER=g++-11 \
      -DCMAKE_CXX_FLAGS="-Werror" \
      -DNimbleSM_ENABLE_UNIT_TESTS=ON \
-     -DNimbleSM_ENABLE_MPI=$NIMBLESM_ENABLE_MPI
+     -DNimbleSM_ENABLE_MPI=$NIMBLESM_ENABLE_MPI \
      -S /opt/src/NimbleSM -B /opt/build/NimbleSM
 cmake --build /opt/build/NimbleSM --parallel $(nproc)
