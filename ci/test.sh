32830b8e2937e218365a849fee69c5414b967ff5
Sean McGovern <st.mcgovern@gmail.com>
2022-07-07 15:27:51 +0000

    #297: use set -e in test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,14 +1,7 @@
+set -e
 . /opt/spack/share/spack/setup-env.sh
 spack env activate nimble
 
 pushd /opt/build/NimbleSM
-testlist=(
-  "ctest --output-on-failure | tee ctest-output.log"
-)
-
-for this in "${testlist[@]}"; do
-  $this || flag=1
-done
-
+ctest --output-on-failure
 popd
-exit $flag
